1313d9a4e4fc69813db5368a8bd3bedf03ca89e1
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-06-30 12:09:13 +0530

    new file:   app1.py modified:   requirements.txt modified:   templates/index.html

---
+++ app1.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, make_response, request, render_template
+import pickle
+import numpy as np
+import os
+
+
+imp_model = pickle.load(open("model.pkl", 'rb'))
+
+def prediction(req):
+  data = req.form or req.get_json(force = True)
+  param1 = data["sepalLength"]
+  param2 = data['sepalWidth']
+  param3 = data['petalLength']
+  param4 = data['petalWidth']
+  predict_on = np.array([[param1, param2, param3, param4]])
+  y_predict = imp_model.predict(predict_on)
+  print(y_predict[0])
+  return(int(y_predict[0]))
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+      if request.form: return(render_template('index.html', results = prediction(request)))
+      else:  return jsonify(results = prediction(request))
+    else: return render_template('index.html', results = None)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+  if request.method == "POST":
+    if request.form: return(render_template('index.html', results = prediction(request)))
+    else:  return jsonify(results = prediction(request))
+  else: return render_template('index.html', results = None)
+
+# app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port= int(os.environ.get("PORT", 8080)))
