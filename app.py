352cfc3de0ddc85d15a1a7aa40fd928e26235353
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2021-05-15 16:18:06 +0530

    added gunicorn

--- app.py
+++ app.py
@@ -4,46 +4,56 @@ import pickle
 import numpy as np
 import os
 
+
 def predictions(req):
-  data = req.form or req.get_json(force = True)
-  param1 = data["sepalLength"]
-  param2 = data['sepalWidth']
-  param3 = data['petalLength']
-  param4 = data['petalWidth']
-  predict_on = np.array([[param1, param2, param3, param4]])
-  y_predict = imp_model.predict(predict_on)
-  print("Prediction:" , int(y_predict[0]))
-  return(y_predict[0])
+    data = req.form or req.get_json(force=True)
+    param1 = data["sepalLength"]
+    param2 = data['sepalWidth']
+    param3 = data['petalLength']
+    param4 = data['petalWidth']
+    predict_on = np.array([[param1, param2, param3, param4]])
+    y_predict = imp_model.predict(predict_on)
+    print("Prediction:", int(y_predict[0]))
+    return(y_predict[0])
+
 
 imp_model = pickle.load(open("model.pkl", 'rb'))
 
 app = Flask(__name__)
 CORS(app)
 
+
 @app.route('/', methods=['GET', "POST"])
 def index():
-  if request.method == 'POST':
-    # return jsonify(results = int(predictions(request)))
-    # return render_template('index.html', results = int(predictions(request)))
-    
-    if request.form: return render_template('index.html', results = int(predictions(request)))
-    else: return jsonify(results = int(predictions(request)))
+    if request.method == 'POST':
+        # return jsonify(results = int(predictions(request)))
+        # return render_template('index.html', results = int(predictions(request)))
 
-  else:
-    return render_template('index.html')
+        if request.form:
+            return render_template('index.html', results=int(predictions(request)))
+        else:
+            return jsonify(results=int(predictions(request)))
+
+    else:
+        return render_template('index.html')
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-  if request.method == 'POST':
-    # return jsonify(results = int(predictions(request)))
+    if request.method == 'POST':
+        # return jsonify(results = int(predictions(request)))
 
-    if request.form: return render_template('index.html', results = int(predictions(request)))
-    else: return jsonify(results = int(predictions(request)))
+        if request.form:
+            return render_template('index.html', results=int(predictions(request)))
+        else:
+            return jsonify(results=int(predictions(request)))
 
-  else:
-    # return ("Server is up and running on /predict ...")
-    return render_template('index.html', results = "undefined")
+    else:
+        # return ("Server is up and running on /predict ...")
+        return render_template('index.html', results="undefined")
+
 
 # app.run()
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= int(os.environ.get("PORT", 8080)))
+    app.run(debug=False if os.environ.get("PORT") else True,
+            port=int(os.environ.get("PORT") or 8080))  # 5000)
--- app1.py
+++ app1.py
@@ -6,33 +6,45 @@ import os
 
 imp_model = pickle.load(open("model.pkl", 'rb'))
 
+
 def prediction(req):
-  data = req.form or req.get_json(force = True)
-  param1 = data["sepalLength"]
-  param2 = data['sepalWidth']
-  param3 = data['petalLength']
-  param4 = data['petalWidth']
-  predict_on = np.array([[param1, param2, param3, param4]])
-  y_predict = imp_model.predict(predict_on)
-  print(y_predict[0])
-  return(int(y_predict[0]))
+    data = req.form or req.get_json(force=True)
+    param1 = data["sepalLength"]
+    param2 = data['sepalWidth']
+    param3 = data['petalLength']
+    param4 = data['petalWidth']
+    predict_on = np.array([[param1, param2, param3, param4]])
+    y_predict = imp_model.predict(predict_on)
+    print(y_predict[0])
+    return(int(y_predict[0]))
+
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-      if request.form: return(render_template('index.html', results = prediction(request)))
-      else:  return jsonify(results = prediction(request))
-    else: return render_template('index.html', results = None)
+        if request.form:
+            return(render_template('index.html', results=prediction(request)))
+        else:
+            return jsonify(results=prediction(request))
+    else:
+        return render_template('index.html', results=None)
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-  if request.method == "POST":
-    if request.form: return(render_template('index.html', results = prediction(request)))
-    else:  return jsonify(results = prediction(request))
-  else: return render_template('index.html', results = None)
+    if request.method == "POST":
+        if request.form:
+            return(render_template('index.html', results=prediction(request)))
+        else:
+            return jsonify(results=prediction(request))
+    else:
+        return render_template('index.html', results=None)
+
 
 # app.run()
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port= int(os.environ.get("PORT", 8080)))
+    app.run(debug=False if os.environ.get("PORT") else True,
+            port=int(os.environ.get("PORT") or 8080))  # 5000)
